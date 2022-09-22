4bc9cd9badb3f64d5b860f1e080a47dcfc50d9c1
Koffepomp <Zephs1986@gmail.com>
2022-09-22 16:01:51 +0200

    klart

--- Pokemon.cs
+++ Pokemon.cs
@@ -8,17 +8,58 @@ namespace Pokedex
 {
     public class Pokemon
     {
-        string name;
+        string name1;
+        string name2;
+        string name3;
         string type;
         int currentForm;
         int totalForms;
+        string desc;
 
-        public Pokemon(string name, string type, int currentForm, int totalForms)
+        public Pokemon(string name1, string name2, string name3, string type, int currentForm, int totalForms, string desc)
         {
-            this.name = name;
+            this.name1 = name1;
+            this.name2 = name2;
+            this.name3 = name3;
             this.type = type;
             this.currentForm = currentForm;
             this.totalForms = totalForms;
+            this.desc = desc;
+        }
+
+        public string Name()
+        {
+            if (currentForm == 1)
+                return name1;
+            else if (currentForm == 2)
+                return name2;
+            else
+                return name3;
+        }
+        public string AllNames()
+        {
+            string allNames = $" ({name1}, {name2}, {name3})";
+            return allNames;
+        }
+
+        public string Type()
+        {
+            return type;
+        }
+
+        public int CurrentForm()
+        {
+            return currentForm;
+        }
+
+        public int TotalForms()
+        {
+            return totalForms;
+        }
+
+        public string Desc()
+        {
+            return desc;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -26,16 +26,101 @@
 
 using Pokedex;
 
+bool running = true;
+
 List<Pokemon> pokedex = new();
-pokedex.Add("Pikachu", "asd", 2, 3);
+pokedex.Add(new Pokemon("Pikachu", "Raichu", "", "Lightning", 1, 2, "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy."));
+pokedex.Add(new Pokemon("Charmander", "Charmeleon", "Charizard", "Fire", 1, 3, "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail."));
+pokedex.Add(new Pokemon("Squirtle", "Wartortle", "Blastoise", "Water", 1, 3, "When it retracts its long neck into its shell, it squirts out water with vigorous force."));
+pokedex.Add(new Pokemon("Bulbasaur", "Ivysaur", "Venusaur", "Grass", 1, 3, "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger."));
+
+while (running)
+{
 
 ShowHeader();
 
+ShowPokedex();
+
+AskUserAction();
+
+}
+
 void ShowHeader()
 {
+    Console.Clear();
     Console.WriteLine("╔═══════════════════════════╗");
     Console.WriteLine("| WELCOME TO KOFFES POKEDEX |");
     Console.WriteLine("╚═══════════════════════════╝");
-    Console.ReadLine();
+}
 
+void ShowPokedex()
+{
+    int pokedexSlot = 1;
+
+    foreach(Pokemon pokemon in pokedex)
+    {
+        Console.WriteLine($"#{pokedexSlot}: {pokemon.Name()}");
+        pokedexSlot++;
+    }
+}
+
+void AskUserAction()
+{
+    Console.Write("\nInsert a number to read more about a pokemon, or add/remove: ");
+    string userInput = Console.ReadLine();
+    if (userInput == "add")
+    {
+        AddPokemonToPokedex();
+    }
+    else if (userInput == "remove")
+    {
+        PickPokemonToRemove();
+    }
+    else
+    {
+        int userInputInt = Convert.ToInt32(userInput);
+        PreviewPokemon(userInputInt);
+    }
+}
+
+void AddPokemonToPokedex()
+{
+    Console.Write("\nWhat's the first stage name of the pokemon? ");
+    string inputName1 = Console.ReadLine();
+    Console.Write("\nWhat's the second stage name of the pokemon? ");
+    string inputName2 = Console.ReadLine();
+    Console.Write("\nWhat's the third stage name of the pokemon? ");
+    string inputName3 = Console.ReadLine();
+    Console.Write("\nWhat's the type of the pokemon? ");
+    string inputType = Console.ReadLine();
+    Console.Write("\nWhat's the current form of the pokemon? ");
+    int inputCurrentForm = Convert.ToInt32(Console.ReadLine());
+    Console.Write("\nWhat's the total number of stages of the pokemon? ");
+    int inputTotalForms = Convert.ToInt32(Console.ReadLine());
+    Console.Write("\nWrite a description of the pokemon: ");
+    string inputDesc = Console.ReadLine();
+
+    pokedex.Add(new Pokemon(inputName1, inputName2, inputName3, inputType, inputCurrentForm, inputTotalForms, inputDesc));
+    Console.WriteLine("\nPokemon successfully added to the pokedex!");
+    Console.ReadLine();
+}
+
+void PickPokemonToRemove()
+{
+    Console.Write("\nWhich pokemon do you want to remove? ");
+    int userIndexRemove = Convert.ToInt32(Console.ReadLine()) - 1;
+    Console.WriteLine($"\n{pokedex[userIndexRemove].Name()} successfully removed from the pokedex!");
+
+    pokedex.Remove(pokedex[userIndexRemove]);
+    Console.ReadLine();
+}
+
+void PreviewPokemon(int userInputInt)
+{
+    int pokedexSlot = userInputInt - 1;
+    Console.WriteLine("\nName: " + pokedex[pokedexSlot].Name() + pokedex[pokedexSlot].AllNames());
+    Console.WriteLine("Type: " + pokedex[pokedexSlot].Type());
+    Console.WriteLine("Evolution Stage: " + pokedex[pokedexSlot].CurrentForm() + "/" + pokedex[pokedexSlot].TotalForms());
+    Console.WriteLine("\nDescription: " + pokedex[pokedexSlot].Desc());
+    Console.ReadLine();
 }
